a1d602ea1ab26f4c6acf4517fb13af6360b228da
Arsalan meo <arsalanmeomeo9@gmail.com>
2024-04-22 16:21:54 +0500

    third  commit

---
+++ main.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: ",
+    },]);
+const words = answer.Sentence.trim().split(" ");
+//print the array of words to console
+console.log(words);
+//print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
+
